4c7b7ad0848f076d144ffc859e5d7ad6c8ec86bb
dstanfor350 <dale.stanford350@outlook.com>
2020-09-26 13:28:39 -0500

    Changed reference to PlayCPP.git

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ touch main.cpp
 git status
 git add main.cpp    // add to staging area
 git commit          // package added files into a commit
-git remote add origin https://github.com/dstanfor350/Playing-in-CPP.git
+git remote add origin https://github.com/dstanfor350/PlayCPP.git
 git push origin master */
 git checkout -b <My branch name>    // create a new branch and move me to it
 git branch          // shows new branch, indicates which branch you are on
