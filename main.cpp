7cbfdccf5b1882c4069b1df7829b377317649b47
dstanfor350 <dale.stanford350@outlook.com>
2020-09-26 13:44:07 -0500

    1st commit to 1stBranch

--- main.cpp
+++ main.cpp
@@ -5,6 +5,10 @@ git status
 git add main.cpp    // add to staging area
 git commit          // package added files into a commit
 git remote add origin https://github.com/dstanfor350/PlayCPP.git
+git pull origin master --allow-unrelated-histories  // had to do this for some reason
 git push origin master */
 git checkout -b <My branch name>    // create a new branch and move me to it
 git branch          // shows new branch, indicates which branch you are on
+git checkout <branchname>   // moves you to <branchname>
+git push origin 1stBranch   // Push a branch to GitHub
+create a pull request: https://github.com/dstanfor350/PlayCPP/pull/new/1stBranch
