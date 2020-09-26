52dc5d08c60cfb8ef5ebf615525430e7ce29491c
dstanfor350 <dale.stanford350@outlook.com>
2020-09-26 13:11:45 -0500

    Initial

---
+++ main.cpp
@@ -0,0 +1,10 @@
+/* cd myproject/
+git init
+touch main.cpp
+git status
+git add main.cpp    // add to staging area
+git commit          // package added files into a commit
+git remote add origin https://github.com/dstanfor350/Playing-in-CPP.git
+git push origin master */
+git checkout -b <My branch name>    // create a new branch and move me to it
+git branch          // shows new branch, indicates which branch you are on
